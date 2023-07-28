76bf5ee1f22e7e06295791026908ff8c8f743582
LuisAdrianCarmona <luisadrian.carmona@hotmail.com>
2023-07-28 16:17:08 -0600

    no se que cambio

--- app.js
+++ app.js
@@ -52,29 +52,6 @@ const juegos = [
 //RUTAS
 //=================================================
 
-/*app.get("/isaac", function (req, res) {
-  res.json(isaac);
-});
-
-app.get("/:juegos", function (req, res) {
-  const chosen = req.params.juegos;
-
-  //logea
-  console.log(chosen);
-
-  res.end();
-});
-
-app.get("/", function (req, res) {
-  res.send("Pagina Principal");
-});
-*/
-
-//manda todo el arreglo de juegos
-/*app.get("/api/juegos", function (req, res) {
-  return res.json(juegos);
-});*/
-
 //manda un juego del arreglo dinamico
 app.get("/api/juegos/:juegos", async function (req, res) {
   //selecciona el juego escogido
@@ -85,13 +62,6 @@ app.get("/api/juegos/:juegos", async function (req, res) {
 
   let games = await JG.find({ routeName: chosen });
   //return res.json(games);
-
-  //Filtro para mostrar el juego
-  /*for (var i = 0; i < juegos.length; i++) {
-    if (chosen === juegos[i].routeName) {
-      return res.json(juegos[i]);
-    }
-  }*/
 
   return res.json(games);
 });
@@ -111,16 +81,15 @@ app.post("/api/juegos", async function (req, res) {
 });
 
 app.put("/api/actualizarEstado", async function (req, res) {
-
-let id = req.body.id
-let state = req.body.state
-console.log(id)
-console.log(state)
-//let game = await JG.find({_id: id})
-//console.log(game)
-//game.state = state
-await JG.updateOne({_id: id}, {state: state})
-res.json(id)
+  let id = req.body.id;
+  let state = req.body.state;
+  console.log(id);
+  console.log(state);
+  //let game = await JG.find({_id: id})
+  //console.log(game)
+  //game.state = state
+  await JG.updateOne({ _id: id }, { state: state });
+  res.json(id);
   // res.json(req.body);
 });
 
@@ -182,14 +151,6 @@ app.get("/juegoEjemplo", function (req, res) {
   res.send("Aqui los juegos ");
   // res.json({ Juego: "Hollow Knight" });
 });
-
-/*app.post("/juegoEjemplo", function (req, res) {
-  let juego = req.body.juego;
-  let gamemode = req.body.gamemode;
-
-  res.send(`Juego: ${juego} gamemode: ${gamemode}`);
-  // res.json(req.body);
-});*/
 
 app.put("/juegos", function (req, res) {
   let juego = req.body.juego;
